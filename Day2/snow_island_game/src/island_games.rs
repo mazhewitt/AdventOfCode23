4fa326f2615244fe903c307c7ab68076f494e95b
mazhewitt <mazda@aduki.co.uk>
2023-12-04 15:53:34 +0100

    cucumber tests pass

---
+++ Day2/snow_island_game/src/island_games.rs
@@ -0,0 +1,197 @@
+// module definition
+
+use std::collections::HashMap;
+use std::str::FromStr;
+use regex::Regex;
+use lazy_static::lazy_static;
+
+// Define a custom error type for parsing
+#[derive(Debug)]
+pub enum GameRevealError {
+    RegexError(String),
+    ParseError(String),
+}
+
+// Lazy static for compiling the regex only once
+lazy_static! {
+    static ref RE: Regex = Regex::new(r"^(\d+) (\w+)$").expect("Invalid regex");
+}
+
+
+#[derive(Debug, PartialEq, Default, Clone)]
+pub struct GameReveal {
+    cube_colour: String,
+    cube_count: u32
+}
+
+impl FromStr for GameReveal {
+    type Err = GameRevealError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let captures = RE.captures(s).ok_or_else(|| GameRevealError::RegexError(s.to_string()))?;
+
+        let cube_count = captures.get(1)
+            .and_then(|m| m.as_str().parse::<u32>().ok())
+            .ok_or_else(|| GameRevealError::ParseError(s.to_string()))?;
+
+        let cube_colour = captures.get(2)
+            .map(|m| m.as_str().to_string())
+            .ok_or_else(|| GameRevealError::ParseError(s.to_string()))?;
+
+        Ok(GameReveal { cube_colour, cube_count })
+    }
+}
+pub fn calculate_possible_games(reveals: HashMap<u32, Vec<GameReveal>>, available_cubes: HashMap<String, u32>) -> Vec<u32> {
+    reveals.into_iter()
+        .filter(|(_, game_reveals)| is_game_possible(game_reveals.to_vec(), available_cubes.clone()))
+        .map(|(game_id, _)| game_id)
+        .collect()
+}
+
+fn is_game_possible(mut reveals: Vec<GameReveal>, mut available_cubes: HashMap<String, u32>) -> bool {
+    if reveals.is_empty() {
+        return true;
+    }
+
+    let reveal = reveals.pop().unwrap();
+
+    match available_cubes.get_mut(&reveal.cube_colour) {
+        Some(available) if *available >= reveal.cube_count => {
+            *available -= reveal.cube_count;
+            is_game_possible(reveals, available_cubes)
+        },
+        _ => false,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashMap;
+
+    #[test]
+    fn test_game_reveal_from_str() {
+        let reveal = GameReveal::from_str("3 blue").unwrap();
+        assert_eq!(reveal, GameReveal {
+            cube_colour: "blue".to_string(),
+            cube_count: 3,
+        });
+    }
+
+    #[test]
+    // test that a game is possible
+    fn test_is_game_possible() {
+        let reveals = vec![
+            GameReveal {
+                cube_colour: "red".to_string(),
+                cube_count: 1,
+            },
+            GameReveal {
+                cube_colour: "green".to_string(),
+                cube_count: 1,
+            },
+            GameReveal {
+                cube_colour: "blue".to_string(),
+                cube_count: 1,
+            },
+        ];
+        let available_cubes = HashMap::from([
+            ("red".to_string(), 1),
+            ("green".to_string(), 3),
+            ("blue".to_string(), 1),
+        ]);
+        assert_eq!(is_game_possible(reveals, available_cubes), true);
+    }
+
+
+
+
+
+    #[test]
+    // negative test that a game is not possible
+    fn test_is_game_not_possible() {
+        let reveals = vec![
+            GameReveal {
+                cube_colour: "red".to_string(),
+                cube_count: 1,
+            },
+            GameReveal {
+                cube_colour: "green".to_string(),
+                cube_count: 5,
+            },
+            GameReveal {
+                cube_colour: "blue".to_string(),
+                cube_count: 1,
+            },
+        ];
+        let available_cubes = HashMap::from([
+            ("red".to_string(), 1),
+            ("green".to_string(), 1),
+            ("blue".to_string(), 1),
+        ]);
+        assert_eq!(is_game_possible(reveals, available_cubes), false);
+    }
+
+    // a test for a game which has two sets of red balls, one set of green balls and one set of blue balls and is not possible
+    #[test]
+    fn test_is_game_not_possible_two_sets_of_red() {
+        let reveals = vec![
+            "1 red".parse::<GameReveal>().unwrap(),
+            "1 red".parse::<GameReveal>().unwrap(),
+            "1 green".parse::<GameReveal>().unwrap(),
+            "1 blue".parse::<GameReveal>().unwrap(),
+        ];
+        let available_cubes = HashMap::from([
+            ("red".to_string(), 1),
+            ("green".to_string(), 1),
+            ("blue".to_string(), 1),
+        ]);
+        assert_eq!(is_game_possible(reveals, available_cubes), false);
+    }
+
+
+
+    // a test for calculate_possible_games
+    #[test]
+    fn test_calculate_possible_games() {
+        let mut reveals = HashMap::new();
+        reveals.insert(1, vec![
+            GameReveal {
+                cube_colour: "red".to_string(),
+                cube_count: 1,
+            },
+            GameReveal {
+                cube_colour: "green".to_string(),
+                cube_count: 1,
+            },
+            GameReveal {
+                cube_colour: "blue".to_string(),
+                cube_count: 1,
+            },
+        ]);
+        reveals.insert(2, vec![
+            GameReveal {
+                cube_colour: "red".to_string(),
+                cube_count: 1,
+            },
+            GameReveal {
+                cube_colour: "green".to_string(),
+                cube_count: 5,
+            },
+            GameReveal {
+                cube_colour: "blue".to_string(),
+                cube_count: 1,
+            },
+        ]);
+        let available_cubes = HashMap::from([
+            ("red".to_string(), 1),
+            ("green".to_string(), 3),
+            ("blue".to_string(), 1),
+        ]);
+        assert_eq!(calculate_possible_games(reveals, available_cubes), vec![1]);
+    }
+
+
+
+
+}
--- Day2/snow_island_game/tests/games.rs
+++ Day2/snow_island_game/tests/games.rs
@@ -1,44 +1,42 @@
-use cucumber::{given, when, then, World};
+use cucumber::{given, then, when, World};
+use std::collections::HashMap;
+use std::str::FromStr;
+use snow_island_game::island_games::{GameReveal, calculate_possible_games};
 
-// These `Cat` definitions would normally be inside your project's code,
-// not test code, but we create them here for the show case.
-#[derive(Debug, Default)]
-struct Cat {
-    pub hungry: bool,
-}
-
-impl Cat {
-    fn feed(&mut self) {
-        self.hungry = false;
-    }
-}
 
 // `World` is your shared, likely mutable state.
 // Cucumber constructs it via `Default::default()` for each scenario.
 #[derive(Debug, Default, World)]
 pub struct CubeGameWorld {
-    red: u32,
-    green: u32,
-    blue: u32
+    available_cubes: HashMap<String, u32>,
+    game_reveals: HashMap<u32, Vec<GameReveal>>
 }
 
 // Steps are defined with `given`, `when` and `then` attributes.
 #[given(expr = "a bag with {int} red cubes, {int} green cubes, {int} blue cubes")]
 fn bag_with(world: &mut CubeGameWorld, red: u32, green: u32, blue: u32) {
-    world.red = red;
-    world.green = green;
-    world.blue = blue;
+    world.available_cubes.insert("red".to_string(), red);
+    world.available_cubes.insert("green".to_string(), green);
+    world.available_cubes.insert("blue".to_string(), blue);
 }
 
-#[when(regex = r"^game (\d) reveals (.+)$")]
+#[when(regex = r"^Game (\d): (.+)$")]
 fn the_game_reveals(world: &mut CubeGameWorld, game: u32, reveals: String){
+    let game_reveals = reveals.split(", ")
+        .map(|reveal| GameReveal::from_str(reveal).unwrap())
+        .collect::<Vec<GameReveal>>();
+
+    world.game_reveals.insert(game, game_reveals);
 
 }
 
 #[then(regex = r"^the possible games are (.*) and the sum is (\d+)$")]
 fn calc_possible_games (world: &mut CubeGameWorld, possible_games: String, sum: u32){
-
-
+    let possible_games = calculate_possible_games(world.game_reveals.clone(), world.available_cubes.clone());
+    // sum possible games
+    let sum_possible_games = possible_games.iter().sum::<u32>();
+    // assert that the sum is correct
+    assert_eq!(sum_possible_games, sum);
 }
 
 // This runs before everything else, so you can setup things here.
