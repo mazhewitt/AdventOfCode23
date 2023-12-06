5e8134a7d440f137bd2cf04c85ed8d0121e8184b
mazhewitt <mazda@aduki.co.uk>
2023-12-06 11:41:21 +0100

    started day 3

--- Day3/scematic/src/main.rs
+++ Day3/scematic/src/main.rs
@@ -1,6 +1,24 @@
+use std::fs;
+use std::fs::File;
+
 fn main() {
     println!("Hello, world!");
 }
+
+fn load_character_grid(filename: &str) -> Vec<Vec<char>> {
+    let mut grid: Vec<Vec<char>> = Vec::new();
+    let mut file = File::open(filename).unwrap();
+    let mut contents = fs::read_to_string(filename).unwrap();
+    for line in contents.lines() {
+        let mut row: Vec<char> = Vec::new();
+        for c in line.chars() {
+            row.push(c);
+        }
+        grid.push(row);
+    }
+    grid
+}
+
 
 //Tests
 #[cfg(test)]
@@ -12,7 +30,8 @@ mod tests {
         // a two dimensional array of characters
 
 
-        let grid:&[&[_]] = load_character_grid("test_file.txt");
-
+        let grid = load_character_grid("test_file.txt");
+        assert_eq!(grid[0].len(), 10);
+        assert_eq!(grid.len(), 10);
     }
 }
