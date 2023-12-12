3d8d51996fdc61e907d85a33c77c69de6668411d
mazda <mazda@aduki.co.uk>
2023-12-12 16:00:00 +0100

    part 2 slow

--- Day12/springs/src/main.rs
+++ Day12/springs/src/main.rs
@@ -1,9 +1,13 @@
 use std::{fs};
 
 fn main() {
-    let test_springs = load_data("real_data.txt");
+    let test_springs = load_data("real_data.txt", false);
     let actual_arrangements = test_springs.iter().map(|(cfg, cond_groups)| find_arrangement_rec(cfg, cond_groups)).sum::<usize>();
     println!("Total Arrangements: {}", actual_arrangements);
+    let expanded_springs = load_data("real_data.txt", true);
+    let expanded_arrangements = expanded_springs.iter().map(|(cfg, cond_groups)| find_arrangement_rec(cfg, cond_groups)).sum::<usize>();
+    println!("Total Arrangements: {}", expanded_arrangements);
+
 }
 
 
@@ -50,12 +54,13 @@ fn find_arrangement_rec(cfg: &str, cond_groups: &Vec<usize>) -> usize {
     count
 }
 
-fn load_data(file: &str) -> Vec<(String, Vec<usize>)> {
+fn load_data(file: &str, should_unfold:bool) -> Vec<(String, Vec<usize>)> {
 
     fs::read_to_string(file)
         .expect("Failed to read file")
         .lines()
-        .map(|line| split_data(line))
+        .map(|line| if should_unfold { unfold(line) } else { line.to_string() })
+        .map(|line| split_data(&line))
         .collect()
 
 }
@@ -67,6 +72,25 @@ fn split_data(line: &str) -> (String, Vec<usize>) {
     (cfg, cond_groups)
 }
 
+fn unfold(line: &str) -> String {
+    let parts: Vec<&str> = line.split_whitespace().collect();
+    if parts.len() != 2 {
+        panic!("Invalid row format");
+    }
+
+    let (cfg, nums) = (parts[0], parts[1]);
+
+    let unfolded_cfg = std::iter::repeat(cfg).take(5).collect::<Vec<&str>>().join("?");
+
+    let nums: Vec<String> = nums.split(',')
+        .map(|num| num.to_string())
+        .collect();
+    let unfolded_nums = nums.join(",");
+    let unfolded_nums = std::iter::repeat(unfolded_nums).take(5).collect::<Vec<String>>().join(",");
+
+    format!("{} {}", unfolded_cfg, unfolded_nums)
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -76,7 +100,7 @@ mod tests {
     #[test]
     fn test_load_data() {
         let file = "test_data.txt";
-        let data = load_data(file);
+        let data = load_data(file, false);
         assert_eq!(data.len(), 6);
     }
 
@@ -91,9 +115,30 @@ mod tests {
 
     #[test]
     fn test_can_find_arrangements_for_multiple_cases() {
-        let test_springs = load_data("test_data.txt");
+        let test_springs = load_data("test_data.txt", false);
         let actual_arrangements = test_springs.iter().map(|(cfg, cond_groups)| find_arrangement_rec(cfg, cond_groups)).sum::<usize>();
         assert_eq!(21, actual_arrangements);
+
+    }
+
+    #[test]
+    fn test_unfold() {
+        let input = ".# 1";
+        let expected = ".#?.#?.#?.#?.# 1,1,1,1,1";
+        assert_eq!(unfold(input), expected);
+
+        let input2 = "???.### 1,1,3";
+        let expected2 = "???.###????.###????.###????.###????.### 1,1,3,1,1,3,1,1,3,1,1,3,1,1,3";
+        assert_eq!(unfold(input2), expected2);
+    }
+
+    #[test]
+    fn test_can_find_arrangements_for_expanded_case() {
+        let test_spring = split_data(&unfold("?###???????? 3,2,1"));
+        let expected_arrangements = 506250;
+
+        let actual_arrangements = find_arrangement_rec(&test_spring.0, &test_spring.1);
+        assert_eq!(expected_arrangements, actual_arrangements);
 
     }
 
