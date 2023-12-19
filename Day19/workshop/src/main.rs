980aae45952d39d3e04db9c8ffede7d18c0537ea
mazhewitt <mazda@aduki.co.uk>
2023-12-19 22:55:46 +0100

    day 19 part 2

--- Day19/workshop/src/main.rs
+++ Day19/workshop/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use regex::Regex;
 
 
@@ -11,7 +12,7 @@ struct WorkFlow {
 struct Rule {
     item: String,
     operator: char,
-    value: i32,
+    value: usize,
     action: String
 }
 
@@ -21,7 +22,7 @@ fn parse_rule(rule_str: &str) -> Result<Rule, String> {
         Ok(Rule {
             item: captures.get(1).unwrap().as_str().to_string(),
             operator: captures.get(2).unwrap().as_str().chars().next().unwrap(),
-            value: captures.get(3).unwrap().as_str().parse::<i32>().map_err(|e| e.to_string())?,
+            value: captures.get(3).unwrap().as_str().parse::<usize>().map_err(|e| e.to_string())?,
             action: captures.get(4).unwrap().as_str().to_string(),
         })
     } else {
@@ -43,13 +44,13 @@ fn parse_workflow(workflow_str: &str) -> Result<WorkFlow, String> {
     Ok(WorkFlow { name: wf_name, rules: parsed_rules, default })
 }
 
-fn load_data(filename: &str) -> (Vec<WorkFlow>, Vec<Vec<(char, usize)>>) {
+fn load_data(filename: &str) -> (HashMap<String, WorkFlow>, Vec<Vec<(char, usize)>>) {
     let file_contents = std::fs::read_to_string(filename).expect("Failed to read file");
     let splitter = Regex::new(r"\r?\n\r?\n").expect("Invalid regex");
     let mut blocks = splitter.split(&file_contents);
     let workflows_block = blocks.next().expect("No workflows block");
     let parts_block = blocks.next().expect("No parts block");
-    let workflows = workflows_block.lines().map(|wf| parse_workflow(wf).unwrap()).collect();
+    let workflows = workflows_block.lines().map(|wf| parse_workflow(wf).unwrap()).map(|wf| (wf.name.clone(), wf)).collect();
     let parts = parts_block.lines().map(|p| parse_parts(p)).collect();
     (workflows, parts)
 }
@@ -60,6 +61,16 @@ fn main() {
     let results = process_parts_through_workflows(&workflows, &parts);
     let sum_of_accepted = results.iter().filter(|(item, _)| item == &'A').map(|(_, value)| value).sum::<usize>();
     println!("Results: {:}", sum_of_accepted);
+    let mut ranges = HashMap::from([
+        ('x', (1, 4000)),
+        ('m', (1, 4000)),
+        ('a', (1, 4000)),
+        ('s', (1, 4000)),
+    ]);
+
+    let total_combinations = calculate_accepted_combinations("in", &workflows, ranges);
+    println!("Total combinations: {}", total_combinations);
+
 }
 fn parse_parts(part_str: &str) -> Vec<(char, usize)> {
     let parse_parts_r = Regex::new(r"^\{x=(\d+),m=(\d+),a=(\d+),s=(\d+)\}$").expect("Invalid regex");
@@ -72,20 +83,20 @@ fn parse_parts(part_str: &str) -> Vec<(char, usize)> {
     ]
 }
 
-fn process_parts_through_workflows(workflows: &[WorkFlow], parts: &Vec<Vec<(char, usize)>>) -> Vec<(char, usize)> {
+fn process_parts_through_workflows(workflows: &HashMap<String, WorkFlow>, parts: &Vec<Vec<(char, usize)>>) -> Vec<(char, usize)> {
     parts.iter().map(|part| {
-        process_part_through_workflows("in".to_string(), workflows, part)
+        process_part_through_workflows("in", workflows, part)
     }).collect()
 }
 
-fn process_part_through_workflows(wf_new:String, workflows: &[WorkFlow], part: &Vec<(char, usize)>) -> (char, usize) {
-    let workflow = workflows.iter().find(|wf| wf.name == wf_new).expect("Workflow not found");
+fn process_part_through_workflows(wf_new:&str, workflows: &HashMap<String, WorkFlow>, part: &Vec<(char, usize)>) -> (char, usize) {
+    let workflow = workflows.get(wf_new).expect("Workflow not found");
     let res = process_workflow(workflow, part);
     let part_total = part.iter().map(|(_, value)| value).sum::<usize>();
     match (res.as_str()) {
         "A" => ('A', part_total),
         "R" => ('R', part_total),
-        _ => process_part_through_workflows(res, workflows, part)
+        _ => process_part_through_workflows(&res, workflows, part)
     }
 }
 
@@ -105,6 +116,38 @@ fn process_workflow(workflow: &WorkFlow, part: &Vec<(char, usize)>) -> String {
     }
     workflow.default.clone()
 }
+
+fn calculate_accepted_combinations(workflow_name: &str, workflows: &HashMap<String, WorkFlow>, mut ranges: HashMap<char, (usize, usize)>) -> usize {
+    match workflow_name {
+        "R" => 0,
+        "A" => ranges.values().map(|&(lo, hi)| hi - lo + 1).product(),
+        _ => {
+            let workflow = &workflows[workflow_name];
+            let mut total = 0;
+            for rule in &workflow.rules {
+                let (lo, hi) = ranges[&rule.item.chars().next().unwrap()];
+                let (t_range, f_range) = if rule.operator == '<' {
+                    ((lo, usize::min(rule.value - 1, hi)), (usize::max(rule.value, lo), hi))
+                } else {
+                    ((usize::max(rule.value + 1, lo), hi), (lo, usize::min(rule.value, hi)))
+                };
+
+                if t_range.0 <= t_range.1 {
+                    let mut copy = ranges.clone();
+                    copy.insert(rule.item.parse().unwrap(), t_range);
+                    total += calculate_accepted_combinations(&rule.action,  workflows, copy);
+                }
+                if f_range.0 <= f_range.1 {
+                    ranges.insert(rule.item.parse().unwrap(), f_range);
+                } else {
+                    break;
+                }
+            }
+            total + calculate_accepted_combinations(&workflow.default,  workflows, ranges)
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -158,7 +201,7 @@ mod tests {
         let filename = "test.txt";
         let (workflows, parts) = load_data(filename);
         let expected = ('R', 4286);
-        let results = process_part_through_workflows("in".to_string(), &workflows, &parts[1]);
+        let results = process_part_through_workflows("in", &workflows, &parts[1]);
         assert_eq!(results, expected);
     }
 
