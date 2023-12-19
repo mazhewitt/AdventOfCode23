76c4526a76d04155eb080cd8e55c93030807cf92
mazda <mazda@aduki.co.uk>
2023-12-19 15:50:52 +0100

    Day 19

--- Day19/workshop/src/main.rs
+++ Day19/workshop/src/main.rs
@@ -15,48 +15,47 @@ struct Rule {
     action: String
 }
 
-fn parse_workflow(workflow_str: &str) -> WorkFlow {
-    //ex{x>10:one,m<20:two,a>30:R,A}
-    let mut workflow = WorkFlow {
-        name: "".to_string(),
-        rules: vec![],
-        default: "".to_string(),
-    };
-    let (wf_name, mut wf_rules) = workflow_str.split_once("{").unwrap();
-    workflow.name = wf_name.to_string();
+fn parse_rule(rule_str: &str) -> Result<Rule, String> {
+    let rule_matcher = Regex::new(r"^(\w)([<>])(\d+):(\w+)$").expect("Invalid regex");
+    if let Some(captures) = rule_matcher.captures(rule_str) {
+        Ok(Rule {
+            item: captures.get(1).unwrap().as_str().to_string(),
+            operator: captures.get(2).unwrap().as_str().chars().next().unwrap(),
+            value: captures.get(3).unwrap().as_str().parse::<i32>().map_err(|e| e.to_string())?,
+            action: captures.get(4).unwrap().as_str().to_string(),
+        })
+    } else {
+        Err("Failed to parse rule".to_string())
+    }
+}
+
+fn parse_workflow(workflow_str: &str) -> Result<WorkFlow, String> {
+    let (wf_name, mut wf_rules) = workflow_str.split_once("{").ok_or("Invalid workflow format")?;
+    let wf_name = wf_name.to_string();
     wf_rules = wf_rules.trim_end_matches("}");
     let mut rules: Vec<_> = wf_rules.split(",").collect();
 
-    // deal with the default rule
-    let default = rules.pop().unwrap();
-    workflow.default = default.to_string();
+    let default = rules.pop().ok_or("No default action in workflow")?.to_string();
+    let parsed_rules = rules.into_iter()
+        .map(|r| parse_rule(r))
+        .collect::<Result<Vec<_>, _>>()?;
 
-    // deal with the rest of the rules
-    for mut rule_str in rules {
-        let rule_matcher = Regex::new(r"^(\w)([<>])(\d+):(\w+)$").expect("Invalid regex");
-        let captures = rule_matcher.captures(rule_str).unwrap();
-
-        let item_str = captures.get(1).unwrap().as_str();
-        let operator_str = captures.get(2).unwrap().as_str();
-        let value_str = captures.get(3).unwrap().as_str();
-        let action_str = captures.get(4).unwrap().as_str();
-
-
-        let rule = Rule {
-            item: item_str.to_string(),
-            operator: operator_str.chars().next().unwrap(),
-            value: value_str.parse::<i32>().unwrap(),
-            action: action_str.to_string()
-        };
-        workflow.rules.push(rule);
-    }
-    workflow
+    Ok(WorkFlow { name: wf_name, rules: parsed_rules, default })
 }
 
 fn main() {
     println!("Hello, world!");
 }
-
+fn parse_parts(part_str: &str) -> Vec<(char, usize)> {
+    let parse_parts_r = Regex::new(r"^\{x=(\d+),m=(\d+),a=(\d+),s=(\d+)\}$").expect("Invalid regex");
+    let captures = parse_parts_r.captures(part_str).expect("Invalid part string");
+    vec![
+        ('x', captures.get(1).unwrap().as_str().parse::<usize>().unwrap()),
+        ('m', captures.get(2).unwrap().as_str().parse::<usize>().unwrap()),
+        ('a', captures.get(3).unwrap().as_str().parse::<usize>().unwrap()),
+        ('s', captures.get(4).unwrap().as_str().parse::<usize>().unwrap()),
+    ]
+}
 
 #[cfg(test)]
 mod tests {
@@ -91,8 +90,44 @@ mod tests {
         };
 
         let actual = parse_workflow(input);
-        assert_eq!(actual, expected);
+        assert_eq!(actual.unwrap(), expected);
     }
 
 
+
+    #[test]
+    fn test_process_part_through_workflows() {
+
+    }
+
+    #[test]
+    fn test_parse_parts(){
+        let part_str = "{x=787,m=2655,a=1222,s=2876}";
+        let expected = vec![
+            ('x', 787),
+            ('m', 2655),
+            ('a', 1222),
+            ('s', 2876),
+        ];
+        let actual = parse_parts(part_str);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_load_data(){
+        let filename = "test.txt";
+
+        let (workflows, parts) = load_data(filename);
+        assert_eq!(workflows.len(), 11);
+        assert_eq!(parts.len(), 5);
+    }
+
+    fn load_data(filename: &str) -> (Vec<WorkFlow>, Vec<Vec<(char, usize)>>) {
+        let file_contents = std::fs::read_to_string(filename).expect("Failed to read file");
+        let workflows_block = file_contents.split("\n\n").next().unwrap();
+        let parts_block = file_contents.split("\n\n").next().unwrap();
+        let workflows = workflows_block.lines().map(|wf| parse_workflow(wf).unwrap()).collect();
+        let parts = parts_block.lines().map(|p| parse_parts(p)).collect();
+        (workflows, parts)
+    }
 }
