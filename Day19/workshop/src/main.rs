8dd73b7dc0819ab8d15bc75099bc1060cf39e856
mazda <mazda@aduki.co.uk>
2023-12-19 17:44:10 +0100

    Day 19 part 1

--- Day19/workshop/src/main.rs
+++ Day19/workshop/src/main.rs
@@ -43,8 +43,23 @@ fn parse_workflow(workflow_str: &str) -> Result<WorkFlow, String> {
     Ok(WorkFlow { name: wf_name, rules: parsed_rules, default })
 }
 
+fn load_data(filename: &str) -> (Vec<WorkFlow>, Vec<Vec<(char, usize)>>) {
+    let file_contents = std::fs::read_to_string(filename).expect("Failed to read file");
+    let splitter = Regex::new(r"\r?\n\r?\n").expect("Invalid regex");
+    let mut blocks = splitter.split(&file_contents);
+    let workflows_block = blocks.next().expect("No workflows block");
+    let parts_block = blocks.next().expect("No parts block");
+    let workflows = workflows_block.lines().map(|wf| parse_workflow(wf).unwrap()).collect();
+    let parts = parts_block.lines().map(|p| parse_parts(p)).collect();
+    (workflows, parts)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "input.txt";
+    let (workflows, parts) = load_data(filename);
+    let results = process_parts_through_workflows(&workflows, &parts);
+    let sum_of_accepted = results.iter().filter(|(item, _)| item == &'A').map(|(_, value)| value).sum::<usize>();
+    println!("Results: {:}", sum_of_accepted);
 }
 fn parse_parts(part_str: &str) -> Vec<(char, usize)> {
     let parse_parts_r = Regex::new(r"^\{x=(\d+),m=(\d+),a=(\d+),s=(\d+)\}$").expect("Invalid regex");
@@ -55,6 +70,40 @@ fn parse_parts(part_str: &str) -> Vec<(char, usize)> {
         ('a', captures.get(3).unwrap().as_str().parse::<usize>().unwrap()),
         ('s', captures.get(4).unwrap().as_str().parse::<usize>().unwrap()),
     ]
+}
+
+fn process_parts_through_workflows(workflows: &[WorkFlow], parts: &Vec<Vec<(char, usize)>>) -> Vec<(char, usize)> {
+    parts.iter().map(|part| {
+        process_part_through_workflows("in".to_string(), workflows, part)
+    }).collect()
+}
+
+fn process_part_through_workflows(wf_new:String, workflows: &[WorkFlow], part: &Vec<(char, usize)>) -> (char, usize) {
+    let workflow = workflows.iter().find(|wf| wf.name == wf_new).expect("Workflow not found");
+    let res = process_workflow(workflow, part);
+    let part_total = part.iter().map(|(_, value)| value).sum::<usize>();
+    match (res.as_str()) {
+        "A" => ('A', part_total),
+        "R" => ('R', part_total),
+        _ => process_part_through_workflows(res, workflows, part)
+    }
+}
+
+fn process_workflow(workflow: &WorkFlow, part: &Vec<(char, usize)>) -> String {
+    for rule in &workflow.rules {
+        if let Some(&(_, part_value)) = part.iter().find(|&&(item, _)| item == rule.item.chars().next().unwrap()) {
+            let condition_met = match rule.operator {
+                '>' => part_value > rule.value as usize,
+                '<' => part_value < rule.value as usize,
+                _ => false, // Handle invalid operator
+            };
+
+            if condition_met {
+                return rule.action.clone();
+            }
+        }
+    }
+    workflow.default.clone()
 }
 
 #[cfg(test)]
@@ -96,8 +145,21 @@ mod tests {
 
 
     #[test]
-    fn test_process_part_through_workflows() {
+    fn test_process_parts_through_workflows() {
+        let filename = "test.txt";
+        let (workflows, parts) = load_data(filename);
+        let expected = vec![('A', 7540), ('R', 4286), ('A', 4623), ('R', 4557), ('A', 6951)];
+        let results = process_parts_through_workflows(&workflows, &parts);
+        assert_eq!(results, expected);
+    }
 
+    #[test]
+    fn test_process_part_through_workflows() {
+        let filename = "test.txt";
+        let (workflows, parts) = load_data(filename);
+        let expected = ('R', 4286);
+        let results = process_part_through_workflows("in".to_string(), &workflows, &parts[1]);
+        assert_eq!(results, expected);
     }
 
     #[test]
@@ -122,12 +184,5 @@ mod tests {
         assert_eq!(parts.len(), 5);
     }
 
-    fn load_data(filename: &str) -> (Vec<WorkFlow>, Vec<Vec<(char, usize)>>) {
-        let file_contents = std::fs::read_to_string(filename).expect("Failed to read file");
-        let workflows_block = file_contents.split("\n\n").next().unwrap();
-        let parts_block = file_contents.split("\n\n").next().unwrap();
-        let workflows = workflows_block.lines().map(|wf| parse_workflow(wf).unwrap()).collect();
-        let parts = parts_block.lines().map(|p| parse_parts(p)).collect();
-        (workflows, parts)
-    }
+
 }
